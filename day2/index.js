8e9cc8616dd8a73daf19e1841306c5960eca5232
JP Hashagen <jayp33e@gmail.com>
2017-10-19 20:59:58 +0200

    Day 2 - Mult output

--- day2/index.js
+++ day2/index.js
@@ -4,8 +4,10 @@ const add = (num1, num2) => num1 + num2;
 
 const mult = (num1, num2) => num1 * num2;
 
-const output = add(3, 5);
+const output_add = add(3, 5);
+const output_mult = mult(3, 5);
 
-console.log(output);
+console.log('Add: ' + output_add);
+console.log('Mult: ' + output_mult);
 
 module.exports = { add:add, mult:mult };
