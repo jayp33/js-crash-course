9ebd3a04ec8e1e4f4627416f26afbc0f652786b2
JP Hashagen <jayp33e@gmail.com>
2017-10-19 20:09:19 +0200

    Day 2 - Mocha

--- day2/index.js
+++ day2/index.js
@@ -2,6 +2,10 @@ console.log('Hello World');
 
 const add = (num1, num2) => num1 + num2;
 
+const mult = (num1, num2) => num1 * num2;
+
 const output = add(3, 5);
 
 console.log(output);
+
+module.exports = { add:add, mult:mult };
---
+++ day2/test/test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+var index = require('../index.js');
+describe('Day2', function() {
+  describe('#add()', function() {
+    it('should return 8 when called with the parameters 3 and 5', function() {
+      assert.equal(8, index.add(3 ,5));
+    });
+  });
+  
+  describe('#mult()', function() {
+    it('should return 15 when called with the parameters 3 and 5', function() {
+      assert.equal(15, index.mult(3 ,5));
+    });
+  });
+});
