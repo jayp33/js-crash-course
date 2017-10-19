bf7f5515194eac416b9d555f22f679c1ae4f966f
JP Hashagen <jayp33e@gmail.com>
2017-10-19 21:21:09 +0200

    Day 2 - Create a Person older than me

--- day2/index.js
+++ day2/index.js
@@ -4,5 +4,8 @@ const Person = require('./person');
 console.log(red('Hello World'));
 
 const jan = new Person('Jan', 39);
+const gandalf = new Person('Gandalf', 99);
 
-console.log(jan);
+const persons = [ jan, gandalf ];
+
+console.log(persons);
