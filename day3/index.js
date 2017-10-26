bc7b32d72d8acfb64e5a0e554b75211d04041d57
JP Hashagen <jayp33e@gmail.com>
2017-10-26 21:13:50 +0200

    Day 3 - Read file contents using chained maps

--- day3/index.js
+++ day3/index.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 
-const filenames = [1,2,3].map(number => `./files/${number}.txt`);
+const files = [1,2,3]
+    .map(number => `./files/${number}.txt`)
+    .map(filename => fs.readFileSync(filename, 'utf8'))
 
-console.log(filenames);
-
-// const text1 = fs.readFile('./files/1.txt', 'utf8');
-// const text2 = fs.readFile('./files/2.txt', 'utf8');
-// const text3 = fs.readFile('./files/3.txt', 'utf8');
-
-// console.log(text1, text2, text3);
+console.log(files);
