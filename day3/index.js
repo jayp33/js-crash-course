ed20f4b08d67e6c27450b35e1edb4b7aa8ca61e4
JP Hashagen <jayp33e@gmail.com>
2017-10-26 21:05:41 +0200

    Day 3 - Read files sync

--- day3/index.js
+++ day3/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
 const text1 = fs.readFileSync('./files/1.txt', 'utf8');
+const text2 = fs.readFileSync('./files/2.txt', 'utf8');
+const text3 = fs.readFileSync('./files/3.txt', 'utf8');
 
-console.log(text1);
+console.log(text1, text2, text3);
