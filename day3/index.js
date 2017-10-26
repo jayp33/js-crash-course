bd042eae968679bbac6b35e03daec562dfa45d0c
JP Hashagen <jayp33e@gmail.com>
2017-10-26 21:07:03 +0200

    Day 3 - Calling an asynchronous function without callback is deprecated

--- day3/index.js
+++ day3/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const text1 = fs.readFileSync('./files/1.txt', 'utf8');
-const text2 = fs.readFileSync('./files/2.txt', 'utf8');
-const text3 = fs.readFileSync('./files/3.txt', 'utf8');
+const text1 = fs.readFile('./files/1.txt', 'utf8');
+const text2 = fs.readFile('./files/2.txt', 'utf8');
+const text3 = fs.readFile('./files/3.txt', 'utf8');
 
 console.log(text1, text2, text3);
