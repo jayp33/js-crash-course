622ae81c28f1045efd2583556b551fbf6d4d4021
JP Hashagen <jayp33e@gmail.com>
2017-10-26 21:24:11 +0200

    Day 3 - Comment

--- day3/index.js
+++ day3/index.js
@@ -1,15 +1,16 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const files = [1,2,3]
-    .map(number => `./files/${number}.txt`)
-    .map(filename => fs.readFileSync(filename, 'utf8'))
+// const files = [1,2,3]
+//     .map(number => `./files/${number}.txt`)
+//     .map(filename => fs.readFileSync(filename, 'utf8'))
 
-console.log(files);
+// console.log(files);
 
-fs.readFile('./files/1.txt', 'utf8', (err, contents) => {
-    console.log(contents)
-    fs.readFile('./files/2.txt', 'utf8', (err, contents) => {
-        console.log(contents)
-        fs.readFile('./files/3.txt', 'utf8', (err, contents) => console.log(contents))
-    })
-})
+// fs.readFile('./files/1.txt', 'utf8', (err, contents) => {
+//     console.log(contents)
+//     fs.readFile('./files/2.txt', 'utf8', (err, contents) => {
+//         console.log(contents)
+//         fs.readFile('./files/3.txt', 'utf8', (err, contents) => console.log(contents))
+//     })
+// })
+
