2d0caaca0723e96e2aaca700ab83cd60b8043e97
JP Hashagen <jayp33e@gmail.com>
2017-10-26 20:42:12 +0200

    Day 2a - Read and write async

--- day2/database.js
+++ day2/database.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 exports.save = (people) => {
-    fs.writeFileSync('./data.json', JSON.stringify(people))
+    fs.writeFile('./data.json', JSON.stringify(people))
     console.log('Saved');
 }
 
 exports.load = (file) => {
-    return JSON.parse(fs.readFileSync(file, 'utf8'));
+    return JSON.parse(fs.readFile(file, 'utf8'));
 }
